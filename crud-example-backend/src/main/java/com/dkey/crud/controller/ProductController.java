b31b2bf0476b88eb28b57bbb4cbaa0d288de9551
dkeyceo <dkeyceo@gmail.com>
2023-07-07 22:44:42 +0300

    Implemented all ProductController methods

--- crud-example-backend/src/main/java/com/dkey/crud/controller/ProductController.java
+++ crud-example-backend/src/main/java/com/dkey/crud/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.dkey.crud.controller;
 
 import com.dkey.crud.dto.Message;
+import com.dkey.crud.dto.ProductDto;
 import com.dkey.crud.entity.Product;
 import com.dkey.crud.service.ProductService;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +34,70 @@ public class ProductController {
         Product product = productService.getOne(id).get();
         return new ResponseEntity<Product>(product, HttpStatus.OK);
     }
+    
+    @GetMapping("/detailname/{name}")
+    public ResponseEntity<Product> getByName(@PathVariable("name") String name){
+        if (!productService.existsByName(name)) {
+            return new ResponseEntity(new Message("No exists"), HttpStatus.NOT_FOUND);
+        }
 
+        Product product = productService.getByName(name).get();
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody ProductDto productDto){
+    	if(StringUtils.isBlank(productDto.getName())) {
+    		return new ResponseEntity(new Message("Name is required"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	if(productDto.getPrice() == null || productDto.getPrice() < 0) {
+    		return new ResponseEntity(new Message("Price must be greater than 0"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	if(productService.existsByName(productDto.getName())) {
+    		return new ResponseEntity(new Message("Name is exists"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	Product product = new Product(productDto.getName(), productDto.getPrice());
+    	productService.save(product);
+    	return new ResponseEntity(new Message("New Product created"), HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody ProductDto productDto){
+    	
+    	if(!productService.existsById(id)) {
+    		return new ResponseEntity(new Message("No product exists for update"), HttpStatus.NOT_FOUND);
+    	}
+    	
+    	if(productService.existsByName(productDto.getName()) && productService.getByName(productDto.getName()).get().getId() != id) {
+    		return new ResponseEntity(new Message("It's name exists"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	if(StringUtils.isBlank(productDto.getName())) {
+    		return new ResponseEntity(new Message("Name is required"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	if(productDto.getPrice() == null || productDto.getPrice() < 0) {
+    		return new ResponseEntity(new Message("Price must be greater than 0"), HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	Product product = productService.getOne(id).get();
+    	product.setName(productDto.getName());
+    	product.setPrice(productDto.getPrice());
+    	productService.save(product);
+    	return new ResponseEntity(new Message("Product updated"), HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+    	
+    	
+    	if(!productService.existsById(id)) {
+    		return new ResponseEntity(new Message("Not found for deleting"), HttpStatus.NOT_FOUND);
+    	}
+    	
+    	productService.delete(id);
+    	return new ResponseEntity(new Message("Product deleted"), HttpStatus.OK);
+    }    
 }
--- crud-example-backend/src/main/java/com/dkey/crud/dto/ProductDto.java
+++ crud-example-backend/src/main/java/com/dkey/crud/dto/ProductDto.java
@@ -16,4 +16,21 @@ public class ProductDto {
         this.name = name;
         this.price = price;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+    
 }
