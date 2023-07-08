a5ec485dc307bff8dc7bcd55da83cbe250084747
dkeyceo <dkeyceo@gmail.com>
2023-07-08 22:50:42 +0300

    Implemented product service

--- crud-example-frontend/src/app/services/product.service.ts
+++ crud-example-frontend/src/app/services/product.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  productURL = 'http://localhost:8080/products/';
+
+  constructor(private http: HttpClient) { }
+
+  public list(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productURL+'list');
+  }
+
+  public details(id: number): Observable<Product> {
+    return this.http.get<Product>(this.productURL+`detail/${id}`);
+  }
+
+  public detailName (name: string): Observable<Product> {
+    return this.http.get<Product>(this.productURL+`detailname/${name}`);
+  }
+
+  public save (product: Product): Observable<any> {
+    return this.http.post<any>(this.productURL+`create`, product);
+  }
+
+  public update (id: number,product: Product): Observable<any> {
+    return this.http.put<any>(this.productURL+`update/${id}`, product);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.http.delete<any>(this.productURL+`delete/${id}`);
+  }
+
+
+
 }
