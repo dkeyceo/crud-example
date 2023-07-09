e571d35e6e1a6ee61dcc7ecbbb6088e98e41d3b2
dkeyceo <dkeyceo@gmail.com>
2023-07-09 22:00:14 +0300

    Added routes, added navbar and described list component

--- crud-example-frontend/src/app/app-routing.module.ts
+++ crud-example-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProductComponent } from './product/create-product.component';
+import { DetailsProductComponent } from './product/details-product.component';
+import { EditProductComponent } from './product/edit-product.component';
+import { ListProductComponent } from './product/list-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ListProductComponent},
+  {path:'details/:id', component: DetailsProductComponent},
+  {path:'create', component: CreateProductComponent},
+  {path:'edit/:id', component: EditProductComponent},
+  {path:'**', redirectTo: '', pathMatch: 'full'}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-example-frontend/src/app/product/list-product.component.ts
+++ crud-example-frontend/src/app/product/list-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-list-product',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts(){
+    this.productService.list().subscribe(data => {
+      this.products = data;
+    }, err =>{
+      console.log(err);
+    });
+  }
+
+  delete(id: number){
+
+  }
 }
